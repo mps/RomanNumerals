c68a8100aa8de544127d03ce3a6268c1e4f5b564
Matthew Strickland <mpstrick@gmail.com>
2012-03-26 08:43:17 -0400

    inital load.

---
+++ RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace RomanNumerals
+{
+    public static class RomanNumerals
+    {
+        public static Dictionary<string, int> Values = new Dictionary<string, int>
+                                                            {
+                                                                { "M", 1000},
+                                                                { "CM", 900},
+                                                                { "D", 500},
+                                                                { "CD", 400},
+                                                                { "C", 100},
+                                                                { "XC", 90},
+                                                                { "L", 50},
+                                                                { "XL", 40},
+                                                                { "X", 10},
+                                                                { "IX", 9},
+                                                                { "V", 5},
+                                                                { "IV", 4},
+                                                                { "I", 1},
+                                                            };
+
+        public static string ToRoman(this int number)
+        {
+            var result = "";
+
+            foreach (var value in Values)
+            {
+                while (number >= value.Value)
+                {
+                    result += value.Key;
+                    number -= value.Value;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ RomanNumeralsTests/Basics.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+using Shouldly;
+
+namespace RomanNumeralsTests
+{
+    [TestFixture]
+    public class Basics
+    {
+        [Test]
+        public void Zero_should_return_nothing()
+        {
+            0.ToRoman().ShouldBe("");
+        }
+
+        [Test]
+        public void One_should_return_I()
+        {
+            1.ToRoman().ShouldBe("I");
+        }
+
+        [Test]
+        public void Two_should_return_II()
+        {
+            2.ToRoman().ShouldBe("II");
+        }
+
+        [Test]
+        public void Three_should_return_III()
+        {
+            3.ToRoman().ShouldBe("III");
+        }
+
+        [Test]
+        public void Four_should_return_IV()
+        {
+            4.ToRoman().ShouldBe("IV");
+        }
+
+        [Test]
+        public void Nine_should_return_IX()
+        {
+            9.ToRoman().ShouldBe("IX");
+        }
+
+        [Test]
+        public void FortyNine_should_return_XLIX()
+        {
+            49.ToRoman().ShouldBe("XLIX");
+        }
+
+        [Test]
+        public void NinetyNine_should_return_XCIX()
+        {
+            99.ToRoman().ShouldBe("XCIX");
+        }
+    }
+}
